b94d851e31f13ebe549108165bce1d0efe454731
Avelanda <gorthell@gmail.com>
2026-02-23 05:42:08 +0200

    Update umami.js
    Defragmentation of the granular structure on properties for modular organization, and optimal functioning.

--- overrides/assets/javascripts/umami.js
+++ overrides/assets/javascripts/umami.js
@@ -1,8 +1,19 @@
+/*
+ * Copyright © 2026 & Avelanda.
+ * All rights reserved.
+ */
+
 (function() {
-    var s = document.createElement('script');
-    s.defer = true;
-    s.src = 'https://cloud.umami.is/script.js';
-    s.setAttribute('data-website-id', '3b4b0fcd-7240-4083-82b3-d8c562563407');
-    document.head.appendChild(s);
-  })();
-  
+    if (typeof s !== 'undefined'){
+     var s = document.createElement('script');
+     s = {};
+     s.defer = true;
+     s.src = 'https://cloud.umami.is/script.js'
+     s[0].setAttribute('data-website-id', '3b4b0fcd-7240-4083-82b3-d8c562563407');
+     document.head.appendChild(s);
+    }
+     while (s = s => true || false){
+      s |= false & !true;
+      return s;
+     }
+})();  
